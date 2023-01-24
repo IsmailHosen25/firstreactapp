0a014488ac6c2371a1009ac4eebb9fb3b52f2643
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-01-24 15:40:06 +0600

    nav added

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from "./components/Nav"
+
+export default function Home() {
+  return (
+    <>
+    <Nav logo="MENTOR" li1="Home" li2="About" li3="Courses" li4="Trainers" li5="Events" li6="Pricing" li7="Drop Down" li7a="Drop Doen 1" li7b="Drop Doen 2" li7c="Drop Doen 3" li7d="Drop Doen 4" li8="Contact" li9="Get Started"/>
+     </>
+  )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./nav.css";
+export default function 
+Nav(props) {
+  return (
+    <div className='main'>
+        <div className='logo'>
+            <h2>{props.logo}</h2>
+        </div>
+        <ul>
+            <li><a href="###" className='act-color'>{props.li1}</a></li>
+            <li><a href="###">{props.li2}</a></li>
+            <li><a href="###">{props.li3}</a></li>
+            <li><a href="###">{props.li4}</a></li>
+            <li><a href="###">{props.li5}</a></li>
+            <li><a href="###">{props.li6}</a></li>
+            <li><a href="###">
+              {props.li7} <i class="fa-solid fa-angle-down"></i></a>
+                <ul>
+                  <li><a href="####">{props.li7a}</a></li>
+                  <li><a href="####">{props.li7b}</a></li>
+                  <li><a href="####">{props.li7c}</a></li>
+                  <li><a href="####">{props.li7d}</a></li>
+                </ul>
+            </li>
+            <li><a href="###">{props.li8}</a></li>
+            <li><button>{props.li9}</button></li>
+            <li id="li10"><i class="fa-solid fa-bars"></i></li>
+        </ul>
+    </div>
+  )
+}
